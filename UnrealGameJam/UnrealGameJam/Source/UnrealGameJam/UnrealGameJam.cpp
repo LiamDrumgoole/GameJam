9ce4f817cc1cfaed3d2826fefb74cbc0987f3bc5
Dylan Alvaro <41766991+DylanAlvaro@users.noreply.github.com>
2023-07-24 10:55:30 +1000

    unreal

---
+++ UnrealGameJam/UnrealGameJam/Source/UnrealGameJam/UnrealGameJam.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGameJam, "UnrealGameJam" );
+ 
