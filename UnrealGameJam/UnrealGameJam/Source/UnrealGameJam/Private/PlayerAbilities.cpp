829f81cfc756307f2cedecf88b6bd9e3149ce935
Dylan Alvaro <41766991+DylanAlvaro@users.noreply.github.com>
2023-07-24 16:18:08 +1000

    - merge

---
+++ UnrealGameJam/UnrealGameJam/Source/UnrealGameJam/Private/PlayerAbilities.cpp
@@ -0,0 +1,36 @@
+#include "PlayerAbilities.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values for this component's properties
+UPlayerAbilities::UPlayerAbilities()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerAbilities::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlayerAbilities::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	zVel = GetOwner()->GetVelocity().Z;
+	
+	if(isGliding && zVel < 0)
+	{
+	}
+}
+
